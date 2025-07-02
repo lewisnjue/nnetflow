094862447456f947e34035eaa131d726d495cea2
lewisnjue <lewiskinyuanjue.ke@gmail.com>
2025-07-02 22:04:43 +0300

    resolved conflict|

--- nnetflow/utils.py
+++ nnetflow/utils.py
@@ -2,7 +2,8 @@ import cupy as cp
 import numpy as np 
 
 
-def is_cuda_available():
+def is_cuda_available() -> bool:
+    """this function is used to check if there is a gpu """
     try:
         cp.cuda.runtime.getDeviceCount()
         return True
