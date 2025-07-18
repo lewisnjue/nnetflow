ca85a3ef5e83a89a957b77f478cb8084fc584b1d
lewisnjue <162419201+lewisnjue@users.noreply.github.com>
2025-07-18 06:33:32 +0000

    still the beginning

---
+++ nnetflow/core/optim/sgd.cpp
@@ -0,0 +1,10 @@
+#include <pybind11/pybind11.h> 
+#include <string> 
+#include <memory>
+#include <vector> 
+class SGD {
+    private:
+        std::vector<Tensor> params; 
+        ... # i will continue from here 
+
+}
