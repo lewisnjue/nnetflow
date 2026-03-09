2afccc3942ba8bbc7d51274c1caa1e4c16fb3444
lewisnjue <162419201+lewisnjue@users.noreply.github.com>
2026-03-09 17:13:07 +0000

    added meta code for what to improve

---
+++ nnetflow/requalizers.py
@@ -0,0 +1,7 @@
+from nnetflow.engine import Tensor 
+
+
+""" 
+ l1,l2, l1_l2, dropout, batchnorm, layernorm,
+  weightnorm, spectralnorm, gradient_clipping, early_stopping
+  """ 
---
+++ nnetflow/scheduler.py
@@ -0,0 +1,24 @@
+from nnetflow.engine import Tensor 
+
+class Scheduler: 
+    def __init__(self): 
+        pass 
+
+
+class Power(Scheduler): 
+    def __init__()
+
+
+class Exponential(Scheduler): 
+    def __init__()
+
+
+
+class PiecewiseConstant(Scheduler): 
+    def __init__() 
+
+class PerformanceScheduler(Scheduler): 
+    def __init__()
+
+class OneCycle(Scheduler): 
+    def __init__()
